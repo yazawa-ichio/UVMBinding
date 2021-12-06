96cd25edc502cd3770d488b28a755e067a9b68f0
yazawa-ichio <yazawa922473@gmail.com>
2021-12-07 02:35:38 +0900

    Unity2021.2でのコンパイルエラーを修正

---
+++ Unity/Assets/UVMBinding/Editor/CodeGen/ReflectionImporterProvider.cs
@@ -0,0 +1,13 @@
+﻿using Mono.Cecil;
+
+namespace UVMBinding.CodeGen
+{
+	internal class ReflectionImporterProvider : IReflectionImporterProvider
+	{
+		public IReflectionImporter GetReflectionImporter(ModuleDefinition moduleDefinition)
+		{
+			return new ReflectionImporter(moduleDefinition);
+		}
+	}
+
+}
