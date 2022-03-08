2dced6a20a2afdefcbc0cbceb2122dc628551ea6
yazawa-ichio <yazawa922473@gmail.com>
2022-03-09 00:40:21 +0900

    対象を取る形に変更

--- Unity/Assets/UVMBinding/uGUI/Binders/TextBinder.cs
+++ Unity/Assets/UVMBinding/uGUI/Binders/TextBinder.cs
@@ -3,24 +3,14 @@ using UnityEngine.UI;
 
 namespace UVMBinding.Binders
 {
-	public class TextBinder : Binder<string>
+	public class TextBinder : Binder<string, Text>
 	{
 		[SerializeField]
 		string m_Format;
 
-		Text m_Text;
-
-		protected override void OnBind()
-		{
-			TryGetComponent(out m_Text);
-		}
-
 		protected override void UpdateValue(string value)
 		{
-			if (m_Text != null)
-			{
-				m_Text.text = string.IsNullOrEmpty(m_Format) ? value : string.Format(m_Format, value);
-			}
+			Target.text = string.IsNullOrEmpty(m_Format) ? value : string.Format(m_Format, value);
 		}
 	}
 }
